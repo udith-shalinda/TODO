46e509752be3f85026a17f33d6263f69f93fcd7b
udith-shalinda <udithshalinda2@gmail.com>
2022-04-02 11:01:26 +0530

    config updated

--- services/axios-client.ts
+++ services/axios-client.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const resolve_accessToken = () => {
-    const bearerToken = localStorage.getItem("token");
-    return bearerToken;
-  };
-  export const save_accessToken = (token: string) => {
-    localStorage.setItem("token", token);
-  };
- 
-  export const axiosClient = axios.create({
-    baseURL: 'http://localhost:8000',
-  });
-  
-  axiosClient.interceptors.request.use(function (config) {
+  const bearerToken = localStorage.getItem("token");
+  return bearerToken;
+};
+export const save_accessToken = (token: string) => {
+  localStorage.setItem("token", token);
+};
+
+export const axiosClient = axios.create({
+  baseURL: "http://localhost:8000/dev",
+});
+
+axiosClient.interceptors.request.use(function (config: any) {
     config.headers.Authorization = `Bearer ${resolve_accessToken()}`;
     return config;
-  });
+});
